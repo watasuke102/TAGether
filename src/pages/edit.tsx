87d903a0e79f7c38d786ba8602e6af471d9bbce5
watasuke102 <watasuke102@gmail.com>
2021-01-08 22:46:58 +0900

    add: edit/profileページを形だけ追加

---
+++ src/pages/edit.tsx
@@ -0,0 +1,16 @@
+// TAGether - Share self-made exam for classmates
+// edit.tsx
+//
+// CopyRight (c) 2020 Watasuke
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT SUSHI-WARE License.
+//
+import React from 'react';
+
+export default () => (
+  <div>
+    <h1>Edit</h1>
+    <p>整備中</p>
+  </div>
+)
