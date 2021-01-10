2a40487c69196d968d573bed0b1625e224af9b8c
watasuke102 <watasuke102@gmail.com>
2021-01-10 23:50:41 +0900

    update: 問題作成ページもコンポーネント化されたtextareaに

--- src/pages/create.tsx
+++ src/pages/create.tsx
@@ -10,6 +10,7 @@ import css from '../style/create.module.css';
 import css_form from '../style/Form.module.css';
 import React from 'react';
 import Button from '../components/Button';
+import Form from '../components/Form';
 import Categoly from '../types/Categoly';
 
 interface Exam {
@@ -17,13 +18,14 @@ interface Exam {
   answer:   string
 }
 
-export default class create extends React.Component {
-  constructor(props) {
-    super(props);
     const categoly_default: Categoly = {
       id: 0, updated_at: '', title: '',
       desc: '', tag: '', list: ''
     }
+
+export default class create extends React.Component {
+  constructor(props) {
+    super(props);
     let tmp: Exam[] = [];
     tmp.push({question: '', answer: ''});
     this.state = {
@@ -51,22 +53,41 @@ export default class create extends React.Component {
     return obj;
   }
 
+  UpdateCategoly(type: string, str: string) {
+    let tmp = categoly_default;
+    switch (type) {
+      case 'title': tmp.title = str; break;
+      case 'desc':  tmp.desc  = str; break;
+      case 'tag':   tmp.tag   = str; break;
+    }
+    this.setState({ categoly: tmp });
+  }
+
   render() {
     return (
       <div className={css.container}>
         <h1>問題の新規登録</h1>
 
         <div className={css.edit_area}>
-          <label>タイトル<textarea className={css_form.form}/></label>
-          <label>説明<textarea className={css_form.form}/></label>
-          <label>タグ (半角コンマ , で区切る)<textarea className={css_form.form}/></label>
+          <Form info={{
+            label: 'タイトル', value: this.state.categoly.title, disabled: false,
+            onChange: (e) => this.UpdateCategoly('title', e.target.value)
+          }}/>
+          <Form info={{
+            label: '説明', value: this.state.categoly.desc, disabled: false,
+            onChange: (e) => this.UpdateCategoly('desc', e.target.value)
+          }}/>
+          <Form info={{
+            label: 'タグ (半角コンマ , で区切る)', value: this.state.categoly.tag, disabled: false,
+            onChange: (e) => this.UpdateCategoly('tag', e.target.value)
+          }}/>
         </div>
 
         <h2>問題</h2>
         {this.ExamEditForm()}
         
         <Button info={{
-          text: "追加", icon: "fas fa-plus",
+          text: "問題の追加", icon: "fas fa-plus",
           onClick: () => this.AddExam(), type: "material"
         }} />
       </div>
