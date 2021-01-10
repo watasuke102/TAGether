8544f1983b3ec6bcf705d0f330b96dfbceb0c883
watasuke102 <watasuke102@gmail.com>
2021-01-10 14:35:40 +0900

    update: 最後の問題で終了ボタンを押したら戻るようにした

--- src/pages/exam.tsx
+++ src/pages/exam.tsx
@@ -8,6 +8,7 @@
 //
 import css from '../style/exam.module.css'
 import React from 'react';
+import Router from 'next/router';
 import { GetServerSideProps } from 'next'
 import Button from '../components/Button';
 
@@ -20,18 +21,24 @@ export default class list extends React.Component {
   }
   // indexを増減する
   IncrementIndex() {
+    // 終了ボタンを押したらlistに戻る
+    if (this.state.index == this.exam.length - 1) {
+      Router.push('/list');
+      return;
+    }
     // 最後の問題であれば、ボタンの内容を変化させる
-    if (this.state.index+1 == this.exam.length - 1)
-    this.setState({ icon: 'check', text: '終了' });
+    if (this.state.index+1 == this.exam.length-1)
+      this.setState({ icon: 'check', text: '終了' });
     this.setState({ index: this.state.index + 1 });
   }
   DecrementIndex() {
     if (this.state.index == 0) return;
     // 最後の問題からひとつ前に戻る時、ボタンの内容をもとに戻す
-    if (this.state.index == this.exam.length - 1)
+    if (this.state.index == this.exam.length-1)
       this.setState({ icon: 'arrow-right', text: '次へ' });
     this.setState({ index: this.state.index - 1 });
   }
+
   // 最初の要素だった場合はボタンを非表示に
   // 次へボタンを右に寄せて表示するため、divを返す
   BackButton() {
@@ -43,6 +50,7 @@ export default class list extends React.Component {
       }} />
     );
   }
+
   render() {
     const index = this.state.index;
     return (
