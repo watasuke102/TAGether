ec5e6a6b1bf31941a3a0557bd013ff60d197ab1d
watasuke102 <watasuke102@gmail.com>
2021-01-11 13:29:22 +0900

    update: 機能要望フォームを形だけ実装

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,35 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
 //
+import css from '../style/index.module.css';
 import React from 'react';
+import Form from '../components/Form';
+import Modal from '../components/Modal';
+import Button from '../components/Button';
+import ModalData from '../types/ModalData';
+
+function SendRequest(s: string, func: Function) {
+  console.log(s);
+  func();
+}
 
 export default function index() {
+  const [request, SetRequest] = React.useState('');
+  const [isModalOpen, SetIsModalOpen] = React.useState(false);
+
+  const modalData: ModalData = {
+    isOpen: isModalOpen,
+    body: (
+      <div className={css.window}>
+        <p>送信しました。<br/>ご協力ありがとうございます。</p>
+        <Button info={{
+          type: 'filled', icon: 'fas fa-times', text: '閉じる',
+          onClick: () => SetIsModalOpen(false)
+        }} />
+      </div>
+    )
+  }
+
   return(
     <div>
       <h1>ようこそ</h1>
@@ -24,6 +50,22 @@ export default function index() {
         <li>カテゴリのお気に入り設定</li>
         <li>その他こまかいとこ</li>
       </ul>
+
+      <h2>機能要望</h2>
+      <p>Webサービスなんもわからないので、意見がほしいです</p>
+      <p>スマホでここが押しづらいとか、表示がわかりにくい等、不満があれば<b>全部</b>書いてください</p>
+      <form className={css.form}>
+        <Form info={{
+          label: '意見', value: request, rows: 10,
+          onChange: (e) => SetRequest(e.target.value)
+        }} />
+        <div className={css.button}> <Button info={{
+          icon: 'fas fa-paper-plane', text: '送信', type: 'filled',
+          onClick: () => SendRequest(request, () => SetIsModalOpen(true))
+        }} /> </div>
+      </form>
+
+      <Modal data={modalData} />
     </div>
   );
 }
