329325acc4b447fb7b69b74a5c1127836ad1e2d3
watasuke102 <watasuke102@gmail.com>
2021-01-11 12:52:27 +0900

    update: indexにそれっぽいこと書いた

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,19 @@ import React from 'react';
 export default function index() {
   return(
     <div>
-      <p>index</p>
+      <h1>ようこそ</h1>
+      <p>「カテゴリ一覧」を選択して問題を解いてみましょう。</p>
+
+      <h2>機能について</h2>
+      <p>以下の機能は未実装ですが、（やる気があれば）実装される予定です。</p>
+      <ul>
+        <li>プロフィール</li>
+        <li>ログイン処理</li>
+        <li>問題の解答履歴（得点など）</li>
+        <li>カテゴリの編集機能</li>
+        <li>カテゴリのお気に入り設定</li>
+        <li>その他こまかいとこ</li>
+      </ul>
     </div>
   );
 }
