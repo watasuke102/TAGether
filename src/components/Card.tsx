bf5575cffd7f553464a5f8dcb86e9ce56da675e2
watasuke102 <watasuke102@gmail.com>
2021-05-09 23:06:39 +0900

    update: カテゴリカードの星の見た目を調整 カードの下に位置するように + お気に入り状態に合わせて色が変わるように

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,13 +11,16 @@ import React from 'react';
 import Tag from './Tag';
 import Modal from './Modal';
 import Detail from './CategolyDetail';
-import { UpdateFavorite } from '../ts/ManageDB';
+import { UpdateFavorite, GetFavorite } from '../ts/ManageDB';
 import Categoly from '../types/Categoly';
 import ModalData from '../types/ModalData';
 
 export default function ExamCard(props: Categoly) {
   const [isModalOpen, setIsModalOpen] = React.useState(false);
   const [favorite_status, SetFavoriteStatus] = React.useState(false);
+  React.useEffect(() => {
+    GetFavorite().then(res => SetFavoriteStatus(res.includes(props.id ?? -1)));
+  }, [])
   // Modalに渡す用のデータ
   const modalData: ModalData = {
     body: <Detail {...{ data: props, close: () => setIsModalOpen(false) }} />,
@@ -44,7 +47,12 @@ export default function ExamCard(props: Categoly) {
         <Tag tag={props.tag} />
         <div
           className={css.favorite_button}
-          onClick={(e) => { e.stopPropagation(); UpdateFavorite(props.id ?? -1); }}
+          style={{ color: favorite_status ? '#c2eb2f' : '#eee' }}
+          onClick={(e) => {
+            e.stopPropagation();
+            UpdateFavorite(props.id ?? -1);
+            SetFavoriteStatus(!favorite_status);
+          }}
         >
           <span className='fas fa-star' />
         </div>
