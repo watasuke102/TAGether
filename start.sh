10b04eeef2721bc6da263f3c3281ef15a8bf6287
watasuke102 <watasuke102@gmail.com>
2021-05-24 20:04:54 +0900

    update: 開発環境でちゃんと起動するようにした+start.shでログを見やすく

--- start.sh
+++ start.sh
@@ -1,14 +1,16 @@
 #!/usr/bin/env bash
 # if you use this under product environment, run "start.sh product"
-set -eu
+set -e
 
 git pull
 
 ## frontend setup
 docker-compose run --rm --entrypoint "npm install" front
+echo -e "\e[36;7m[info] Frontend setup completed\e[m"
 
 ## backend setup
 docker-compose run --rm --entrypoint "composer install" back
+echo -e "\e[36;7m[info] Backend setup completed\e[m"
 
 ## when product environment
 if [[ ${1} == "product" ]]; then
@@ -17,5 +19,7 @@ if [[ ${1} == "product" ]]; then
   docker-compose -f docker-compose.yml -f product.override.yml up -d --build
 else
   ## when develop environment
-  docker-compose -d --build
+  docker-compose up -d --build
 fi
+
+echo -e "\e[36;7m[info] Start complete\e[m"
