443d821ff69e93a328a36822d47692aab91b6c2d
watasuke102 <watasuke102@gmail.com>
2021-10-26 17:07:11 +0900

    update: CSSをコンポーネント毎に分割

--- front/src/components/ExamEditForms.tsx
+++ front/src/components/ExamEditForms.tsx
@@ -6,7 +6,7 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT SUSHI-WARE License.
 //
-import css from '../style/pages/create.module.scss';
+import css from '../style/components/ExamEditForms.module.scss';
 import React from 'react';
 import Form from "./Form";
 import Button from "./Button";
