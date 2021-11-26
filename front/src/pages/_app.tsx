c91c4393d4929e805f50463c2294d1094e924d9d
watasuke102 <watasuke102@gmail.com>
2021-11-26 13:22:11 +0900

    update: transitionが動かなくなったのでローディングバーを付けた

--- front/src/pages/_app.tsx
+++ front/src/pages/_app.tsx
@@ -7,28 +7,27 @@
 // This software is released under the MIT SUSHI-WARE License.
 //
 import '../style/main.scss';
-import React from 'react';
 import Head from 'next/head';
-import App, { Container } from 'next/app';
-import PageTransition from '../ts/next-page-transisions/PageTransition';
+import React from 'react';
+import NProgress from 'nprogress';
+import { AppProps } from 'next/app';
+import '../style/nprogress.css';
 
 import Header from '../components/Header';
 
-export default class MyApp extends App {
-  render(): React.ReactElement {
-    const { Component, pageProps, router } = this.props;
-    return (
-      <main>
-        <Head>
-          <title>TAGether</title>
-        </Head>
-        <Header />
-        <Container>
-          <PageTransition timeout={300} classNames="page-transition">
-            <Component {...pageProps} key={router.route} />
-          </PageTransition>
-        </Container>
-      </main>
-    );
+export default function MyApp({ Component, pageProps, router }: AppProps): React.ReactElement {
+  if (router.events) {
+    router.events.on('routeChangeStart', () => NProgress.start());
+    router.events.on('routeChangeComplete', () => NProgress.done());
+    router.events.on('routeChangeError', () => NProgress.done());
   }
+  return (
+    <main>
+      <Head>
+        <title>TAGether</title>
+      </Head>
+      <Header />
+      <Component {...pageProps} key={router.route} />
+    </main >
+  );
 }
