853456cf83327b984acf634a8958da37f5d80fd8
watasuke102 <watasuke102@gmail.com>
2022-01-09 14:17:41 +0900

    add: 読み込み中に表示するLoadingコンポーネント

---
+++ front/src/components/common/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+// TAGether - Share self-made exam for classmates
+// Loading.tsx
+//
+// CopyRight (c) 2020-2021 Watasuke
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT SUSHI-WARE License.
+//
+import React from 'react';
+
+export default function Loading(): React.ReactElement {
+  return <span>Loading...</span>;
+}
