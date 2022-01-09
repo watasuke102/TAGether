9c9a67f0b169df1d731b384d614e82b9671ce5d7
watasuke102 <watasuke102@gmail.com>
2022-01-09 22:43:48 +0900

    update: ExamTableComponentをFunctionComponentに

--- front/src/components/features/ExamTable/ExamTableComponent.tsx
+++ front/src/components/features/ExamTable/ExamTableComponent.tsx
@@ -18,114 +18,104 @@ interface Props {
   examState?: ExamState[];
   answers?: string[][];
 }
-interface State {
-  array: number[];
-}
 
-export default class ExamTable extends React.Component<Props, State> {
-  constructor(props: Props) {
-    super(props);
-    // 連番配列を作成
-    let array: number[] = [];
-    for (let i = 0; i < this.props.exam.length; i++) array.push(i);
-    // examStateのorder順にソート
-    if (this.props.examState) {
-      const first: number[] = [];
-      const second: number[] = [];
-      const third: number[] = array;
+export default function ExamTable(props: Props): React.ReactElement {
+  // 連番配列を作成
+  let index: number[] = [];
+  for (let i = 0; i < props.exam.length; i++) index.push(i);
+  // examStateのorder順にソート
+  if (props.examState) {
+    const first: number[] = [];
+    const second: number[] = [];
+    const third: number[] = index;
 
-      this.props.examState.forEach((e, i) => {
-        switch (e.order) {
-          case 2:
-            first.push(third[i]);
-            third[i] = -1;
-            break;
-          case 1:
-            second.push(third[i]);
-            third[i] = -1;
-            break;
-          case 0:
-            break;
-        }
-      });
-      // 連結する
-      // 条件に合うorderがなかった場合、配列がundefinedになるため、それをfilterで削除
-      array = first.concat(second.concat(third)).filter(e => e !== undefined && e !== -1);
-    }
-    this.state = {array: array};
+    props.examState.forEach((e, i) => {
+      switch (e.order) {
+        case 2:
+          first.push(third[i]);
+          third[i] = -1;
+          break;
+        case 1:
+          second.push(third[i]);
+          third[i] = -1;
+          break;
+        case 0:
+          break;
+      }
+    });
+    // 連結する
+    // 条件に合うorderがなかった場合、配列がundefinedになるため、それをfilterで削除
+    index = first.concat(second.concat(third)).filter(e => e !== undefined && e !== -1);
   }
 
-  Status(i: number): React.ReactElement | undefined {
-    if (!this.props.examState || !this.props.answers) return;
+  function Status(i: number): React.ReactElement | undefined {
+    if (!props.examState || !props.answers) return;
     // 正解or不正解、もしくはn問正解の表示
-    const count = this.props.examState[i].correctAnswerCount;
+    const count = props.examState[i].correctAnswerCount;
     let correct_state: string = '';
-    if (this.props.exam[i].answer.length === 1) {
+    if (props.exam[i].answer.length === 1) {
       correct_state = count === 1 ? '正解' : '不正解';
     } else {
       correct_state = count + '問正解';
     }
     return (
       <>
-        <td>{ParseAnswers(this.props.answers[i], this.props.exam[i])}</td>
+        <td>{ParseAnswers(props.answers[i], props.exam[i])}</td>
         <td>
           <span key={`state_${i} `}>{correct_state}</span>
         </td>
       </>
     );
   }
-
-  render(): React.ReactElement {
-    const list: React.ReactElement[] = [];
-    const exam = this.props.exam;
-    this.state.array.forEach(i => {
-      list.push(
-        <tr key={`item_${i} `}>
-          <td>
-            {
-              // 問題
-              exam[i].question.split('\n').map(str => {
-                return (
-                  <span key={`q_${i} `}>
-                    {str}
-                    <br />
-                  </span>
-                );
-              })
-            }
-          </td>
-          <td className={this.props.showCorrectAnswer ? '' : css.hide_correct_answer}>
-            {ParseAnswers(exam[i].answer, this.props.exam[i])}
-          </td>
-          {/* 自分の解答+何問正解したか */ this.Status(i)}
-        </tr>,
-      );
-    });
-    let state_th: React.ReactElement = <></>;
-    if (this.props.examState && this.props.answers)
-      state_th = (
-        <>
-          <th>自分の解答</th>
-          <th>状態</th>
-        </>
-      );
-    return (
+  const list: React.ReactElement[] = [];
+  const exam = props.exam;
+  index.forEach(i => {
+    list.push(
+      <tr key={`item_${i} `}>
+        <td>
+          {
+            // 問題
+            exam[i].question.split('\n').map(str => {
+              return (
+                <span key={`q_${i} `}>
+                  {str}
+                  <br />
+                </span>
+              );
+            })
+          }
+        </td>
+        <td className={props.showCorrectAnswer ? '' : css.hide_correct_answer}>
+          {ParseAnswers(exam[i].answer, props.exam[i])}
+        </td>
+        {/* 自分の解答+何問正解したか */ Status(i)}
+      </tr>,
+    );
+  });
+  let state_th: React.ReactElement = <></>;
+  if (props.examState && props.answers)
+    state_th = (
       <>
-        <div className={css.table}>
-          <table>
-            <tbody>
-              <tr>
-                <th>問題</th>
-                <th>正解</th>
-                {state_th}
-              </tr>
-              {list}
-            </tbody>
-          </table>
-        </div>
-
-        <div className={css.bottom} />
+        <th>自分の解答</th>
+        <th>状態</th>
       </>
     );
-  }
+  return (
+    <>
+      <div className={css.table}>
+        <table>
+          <tbody>
+            <tr>
+              <th>問題</th>
+              <th>正解</th>
+              {state_th}
+            </tr>
+            {list}
+          </tbody>
+        </table>
+      </div>
+
+      <div className={css.bottom} />
+    </>
+  );
 }
