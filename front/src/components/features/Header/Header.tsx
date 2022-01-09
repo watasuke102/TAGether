6abd2a51ab184b7cee482c9629a98354323b2614
watasuke102 <watasuke102@gmail.com>
2022-01-09 22:40:29 +0900

    update: HeaderをFunction Componentにした

--- front/src/components/features/Header/Header.tsx
+++ front/src/components/features/Header/Header.tsx
@@ -12,61 +12,43 @@ import React from 'react';
 import Button from '@/common/Button/Button';
 import ButtonInfo from '@mytypes/ButtonInfo';
 
-interface State {
-  isFixedButtons: boolean;
-}
+export default function Header(): React.ReactElement {
+  const [isFixedButtons, SetIsFixedButtons] = React.useState(false);
 
-// TODO: どうにかならないかなぁ
-/* eslint @typescript-eslint/no-explicit-any: 0 */
-export default class Header extends React.Component<any, State> {
-  constructor(props: State) {
-    super(props);
-    this.state = {isFixedButtons: false};
-  }
-
-  componentDidMount(): void {
-    window.addEventListener('scroll', () => this.SetIsFixedButtons(), true);
-  }
-  componentWillUnmount(): void {
-    window.removeEventListener('scroll', () => this.SetIsFixedButtons(), true);
-  }
-
-  SetIsFixedButtons(): void {
+  const onScroll = (): void => {
     if (!process.browser) return;
-    let isFixedButtons = false;
-    if (window.pageYOffset > 70) {
-      isFixedButtons = true;
-    }
-    this.setState({isFixedButtons: isFixedButtons});
-  }
+    SetIsFixedButtons(window.pageYOffset > 70);
+  };
 
-  ButtonContainersCSS(): string {
-    if (!this.state.isFixedButtons) return css.buttons_container;
+  React.useEffect(() => {
+    window.addEventListener('scroll', onScroll, true);
+    return () => window.removeEventListener('scroll', onScroll, true);
+  }, []);
+
+  function ButtonContainersCSS(): string {
+    if (!isFixedButtons) return css.buttons_container;
     return css.buttons_container_fixed;
   }
 
-  render(): React.ReactElement {
-    const info: ButtonInfo[] = [];
-    // prettier-ignore
-    {
-      info.push({ text: 'ホーム', icon: 'fas fa-home', onClick: () => Router.push('/'), type: 'icon_desc' });
-      info.push({ text: 'カテゴリ一覧', icon: 'fas fa-book', onClick: () => Router.push('/list'), type: 'icon_desc' });
-      info.push({ text: 'タグ一覧', icon: 'fas fa-tag', onClick: () => Router.push('/tag'), type: 'icon_desc' });
-      info.push({ text: 'プロフィール', icon: 'fas fa-user', onClick: () => Router.push('/profile'), type: 'icon_desc' });
-      info.push({ text: '機能要望', icon: 'fas fa-comment-alt', onClick: () => Router.push('/request'), type: 'icon_desc' });
-    }
-    const button_list: React.ReactElement[] = [];
-    info.forEach(element => {
-      button_list.push(<Button key={`headerbutton_${element.text}`} {...element} />);
-    });
+  // prettier-ignore
+  const info: ButtonInfo[] = [
+    { text: 'ホーム',       icon: 'fas fa-home',        onClick: () => Router.push('/'),        type: 'icon_desc' },
+    { text: 'カテゴリ一覧', icon: 'fas fa-book',        onClick: () => Router.push('/list'),    type: 'icon_desc' },
+    { text: 'タグ一覧',     icon: 'fas fa-tag',         onClick: () => Router.push('/tag'),     type: 'icon_desc' },
+    { text: 'プロフィール', icon: 'fas fa-user',        onClick: () => Router.push('/profile'), type: 'icon_desc' },
+    { text: '機能要望',     icon: 'fas fa-comment-alt', onClick: () => Router.push('/request'), type: 'icon_desc' },
+  ];
 
-    return (
-      <header className={css.header}>
-        <h1>TAGether</h1>
-        <nav className={this.ButtonContainersCSS()}>
-          <div className={css.buttons}>{button_list}</div>
-        </nav>
-      </header>
-    );
-  }
+  return (
+    <header className={css.header}>
+      <h1>TAGether</h1>
+      <nav className={ButtonContainersCSS()}>
+        <div className={css.buttons}>
+          {info.map(e => (
+            <Button key={`headerbutton_${e.text}`} {...e} />
+          ))}
+        </div>
+      </nav>
+    </header>
+  );
 }
