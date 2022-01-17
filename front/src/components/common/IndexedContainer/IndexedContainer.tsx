de27949cf8abfa4cde4c45fb12c5da6fb5bfcf58
watasuke102 <watasuke102@gmail.com>
2022-01-17 18:15:44 +0900

    fix: IndexedContainerで子要素が0個のときはメッセージを表示するように
    0個のとき無限にページ移動できてしまう問題を修正

--- front/src/components/common/IndexedContainer/IndexedContainer.tsx
+++ front/src/components/common/IndexedContainer/IndexedContainer.tsx
@@ -23,16 +23,18 @@ export default function SelectButton(props: Props): React.ReactElement {
   const max_page = Math.ceil(props.len / per);
   const width_var = {'--item_width': `${props.width ?? '1fr'}`} as React.CSSProperties;
 
+  if (props.children.length < 1) return <span>何もありません</span>;
+
   return (
     <>
       <div className={css.operator}>
-        <Button icon='fas fa-angle-left' onClick={() => index !== 0 && SetIndex(i => i - 1)} type='material' text='' />
+        <Button icon='fas fa-angle-left' onClick={() => index > 0 && SetIndex(i => i - 1)} type='material' text='' />
         <span>
           {index + 1}/{max_page}
         </span>
         <Button
           icon='fas fa-angle-right'
-          onClick={() => index + 1 !== max_page && SetIndex(i => i + 1)}
+          onClick={() => index + 1 < max_page && SetIndex(i => i + 1)}
           type='material'
           text=''
         />
