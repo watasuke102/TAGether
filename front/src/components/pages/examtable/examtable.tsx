cd7f4bb9931ebadc14acf7bf56d3f3a1c35c1d99
watasuke102 <watasuke102@gmail.com>
2022-08-06 11:04:09 +0900

    update: examtable>カテゴリの情報を表示するようにした

--- front/src/components/pages/examtable/examtable.tsx
+++ front/src/components/pages/examtable/examtable.tsx
@@ -25,6 +25,7 @@ interface Props {
 export default function ExamTable(props: Props): React.ReactElement {
   const [showCorrectAnswer, SetShowCorrectAnswer] = React.useState(false);
 
+  const rate = props.history && Math.round((props.history.correct_count / props.history.total_question) * 10000) / 100;
   const exam: Exam[] = JSON.parse(props.data.list);
   // 読み込みが終わっていなかった場合
   if (exam.length === 0 && props.history) {
@@ -34,28 +35,31 @@ export default function ExamTable(props: Props): React.ReactElement {
   return (
     <>
       <Helmet title={`問題一覧 : ${props.data.title} - TAGether`} />
+
+      <div className={css.examdata_container}>
+        <span className={css.title}>{props.data.title}</span>
+        <div className={css.correct_rate_statuslist}>
+          {props.history && (
+            <span>
+              {props.history.total_question}問中{props.history.correct_count}問正解、 正答率{rate}%
+            </span>
+          )}
+        </div>
+      </div>
+
       <div className={css.table}>
         <ExamTableComponent exam={exam} showCorrectAnswer={showCorrectAnswer} />
       </div>
 
       <div className={css.button_container}>
         <div className={css.buttons}>
-          <Button
-            {...{
-              text: '戻る',
-              icon: 'fas fa-undo',
-              onClick: Router.back,
-              type: 'material',
-            }}
-          />
+          <Button text='戻る' icon='fas fa-undo' onClick={Router.back} type='material' />
           {/* 正しい答えの表示/非表示切り替え */}
           <Button
-            {...{
-              onClick: () => SetShowCorrectAnswer(!showCorrectAnswer),
-              type: 'material',
-              text: showCorrectAnswer ? '正解を非表示' : '正解を表示',
-              icon: showCorrectAnswer ? 'fas fa-eye-slash' : 'fas fa-eye',
-            }}
+            onClick={() => SetShowCorrectAnswer(!showCorrectAnswer)}
+            type='material'
+            text={showCorrectAnswer ? '正解を非表示' : '正解を表示'}
+            icon={showCorrectAnswer ? 'fas fa-eye-slash' : 'fas fa-eye'}
           />
         </div>
       </div>
