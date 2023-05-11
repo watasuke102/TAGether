0b12af4d7f026e0091c234c2cf63dc8d562f85af
watasuke102 <watasuke102@gmail.com>
2023-05-11 23:37:52 +0900

    update: start.sh>起動コマンドをちょっとだけ変えた

--- start.sh
+++ start.sh
@@ -19,21 +19,21 @@ fi
 echo -e "\e[32;7m[info] Started setup script (env: ${1:-not_product})\e[m"
 
 ## frontend setup
-docker-compose run --rm --entrypoint "npm install" front
+docker compose run --rm --entrypoint "npm install" front
 echo -e "\e[36;7m[info] Frontend setup completed\e[m"
 
 ## backend setup
-docker-compose run --rm --entrypoint "npm install" back
+docker compose run --rm --entrypoint "npm install" back
 echo -e "\e[36;7m[info] Backend setup completed\e[m"
 
 ## when product environment
 if [[ ${1} == "product" ]]; then
-  docker-compose run --rm --entrypoint "npm run build" front
+  docker compose run --rm --entrypoint "npm run build" front
   ## start
-  docker-compose -f docker-compose.yml -f product.override.yml up -d --build
+  docker compose -f docker-compose.yml -f product.override.yml up -d --build
 else
   ## when develop environment
-  docker-compose up -d --build
+  docker compose up -d --build
 fi
 
 echo -e "\e[36;7m[info] Setup was completed\e[m"
