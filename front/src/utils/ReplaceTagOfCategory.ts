805dd4dce8981bd01acf7292e859f79436fce570
watasuke102 <watasuke102@gmail.com>
2023-12-23 00:48:08 +0900

    fix: ReplaceTagOfCategory>型にanyを使ってたのが微妙だったので直した

--- front/src/utils/ReplaceTagOfCategory.ts
+++ front/src/utils/ReplaceTagOfCategory.ts
@@ -6,30 +6,27 @@
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import TagData from '@mytypes/TagData';
 
-type AnyWithTags = {
-  tag: string;
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  [key: string]: any;
-};
-
 // '1,2,3' のように、タグのIDをカンマ区切りで格納しているCategoryに対して、
 // IDと対応するタグのデータに置き換える
-export function replace_tag_of_category(categories: AnyWithTags[], tags: TagData[]) {
+export function replace_tag_of_category<T extends {tag: string}>(
+  categories: T[],
+  tags: TagData[],
+): (Omit<T, 'tag'> & {tag: TagData[]})[] {
   const tag_map = new Map(tags.map(e => [e.id, e]));
   return categories.map(category => {
     const tag =
       // prettier-ignore
       category.tag === ''? [] : category.tag.split(',').map(tag_item => {
-          const tag = tag_map.get(Number(tag_item));
-          if (tag) {
-            return tag;
-          }
-          return {
-            name: tag_item,
-            description: '',
-            updated_at: '',
-          };
-        });
+        const tag = tag_map.get(Number(tag_item));
+        if (tag) {
+          return tag;
+        }
+        return {
+          name: tag_item,
+          description: '',
+          updated_at: '',
+        };
+      });
     return {...category, tag: tag};
   });
 }
