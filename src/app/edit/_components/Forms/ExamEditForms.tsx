10e8d5b15eb6e60ff1f4f3db95b0b339420e2e7f
watasuke102 <watasuke102@gmail.com>
2023-12-28 18:51:42 +0900

    update: ExamEditForms>appディレクトリ下に移動

--- src/app/edit/_components/Forms/ExamEditForms.tsx
+++ src/app/edit/_components/Forms/ExamEditForms.tsx
@@ -5,7 +5,7 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import css from './ExamEditForms.module.scss';
-import {ExamContext} from 'src/app/edit/page';
+import {ExamContext} from '../../page';
 import {DragDropContext, Droppable, Draggable, DropResult} from '@hello-pangea/dnd';
 import React from 'react';
 import Button from '@/common/Button/Button';
