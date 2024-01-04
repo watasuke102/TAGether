48169c6b9446b5d61c349ab735bf1cc978dfed4b
watasuke102 <watasuke102@gmail.com>
2024-01-05 00:20:31 +0900

    fix: eslintなどが動作していなかった

--- src/app/edit/loading.tsx
+++ src/app/edit/loading.tsx
@@ -1,3 +1,9 @@
+// TAGether - Share self-made exam for classmates
+// CopyRight (c) 2020-2023 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT or MIT SUSHI-WARE License.
 import Loading from '@/common/Loading/Loading';
 
 export default Loading;
