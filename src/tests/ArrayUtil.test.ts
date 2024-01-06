43192eb85cab10d26472084d9a572f45a3eb7e0f
watasuke102 <watasuke102@gmail.com>
2024-01-06 18:28:55 +0900

    add: ArrayUtil>TogglleElement

--- src/tests/ArrayUtil.test.ts
+++ src/tests/ArrayUtil.test.ts
@@ -5,7 +5,7 @@
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import {describe, expect, test} from 'vitest';
-import {Move, Swap} from '../utils/ArrayUtil';
+import {Move, Swap, ToggleElement} from '../utils/ArrayUtil';
 
 describe('Move', () => {
   const array = [1, 2, 3, 4, 5];
@@ -19,4 +19,19 @@ describe('Swap', () => {
   const changed = Swap(array, 0, 4);
   test('要素が入れ替わっている', () => expect(changed).toStrictEqual([5, 2, 3, 4, 1]));
   test('元の配列は不変である', () => expect(array).toStrictEqual([1, 2, 3, 4, 5]));
+});
+
+describe('ToggleElement', () => {
+  const array = [1, 2, 3];
+  const first = ToggleElement(array, 4);
+  test('配列に含まれない要素を渡すと、その要素を含む配列が返される', () => {
+    expect(array.indexOf(4)).toBe(-1);
+    expect(first).toStrictEqual([1, 2, 3, 4]);
+  });
+  test('元の配列は不変である', () => expect(array).toStrictEqual([1, 2, 3]));
+  const second = ToggleElement(first, 4);
+  test('配列に含まれる要素を渡すと、その要素を含まない配列が返される', () => {
+    expect(first.indexOf(4)).not.toBe(-1);
+    expect(second).toStrictEqual([1, 2, 3]);
+  });
 });
