ffb56bf59b0e377e29b6f64e09f037809493ed18
watasuke102 <watasuke102@gmail.com>
2024-01-07 17:23:21 +0900

    update: ExamAnswerArea>ページ切替時の自動フォーカス

--- src/app/exam/_components/ExamAnswerArea/ExamAnswerArea.tsx
+++ src/app/exam/_components/ExamAnswerArea/ExamAnswerArea.tsx
@@ -12,10 +12,96 @@ import Form from '@/common/TextForm/Form';
 import {SelectButton} from '@/common/SelectBox';
 import ListIcon from '@assets/list.svg';
 
+const TEXT_FIRST_FORM_ID = 'TextExamFirstForm';
+const SELECT_FIRST_ITEM_ID = 'SelectExamFirstItem';
+const SORT_FIRST_ITEM_ID = 'SortExamFirstItem';
+
 export function ExamAnswerArea(): JSX.Element {
   const [state, dispatch] = React.useContext(ExamReducerContext);
   const exam = state.exam[state.index];
   const status = state.exam_state[state.index];
+
+  // keyboard shortcut
+  React.useEffect(() => {
+    const Shortcut = (e: KeyboardEvent) => {
+      if (!e.ctrlKey || !e.shiftKey || e.repeat || status.checked) {
+        return;
+      }
+      if (exam.type === 'Select' || exam.type === 'MultiSelect') {
+        const num = e.code.match(/(Digit|Numpad)([1-9])/);
+        if (!num || !num[2]) {
+          return;
+        }
+        e.preventDefault();
+        dispatch({type: exam.type === 'Select' ? 'select/set' : 'multi/toggle', index: Number(num[2]) - 1});
+        return;
+      }
+
+      if (exam.type === 'Sort') {
+        let direction = 0;
+        // Ctrl + Shift + ↓↑ or jkで動かす
+        switch (e.code) {
+          case 'KeyJ':
+          case 'ArrowDown':
+            direction = 1;
+            break;
+          case 'KeyK':
+          case 'ArrowUp':
+            direction = -1;
+            break;
+          default:
+            return;
+        }
+        e.preventDefault();
+        // 一つ上下に移動させる
+        const from_element = document.activeElement;
+        const index = Number(from_element?.attributes.getNamedItem('sort_index')?.value);
+        if (!Number.isInteger(index)) return;
+        const to = index + direction;
+        if (to < 0 || to >= exam.answer.length) return;
+        dispatch({type: 'sort/move', from: index, to});
+        // 移動先の要素にフォーカスする
+        const to_element = document.querySelector<HTMLElement>(`[sort_index="${to}"]`);
+        to_element?.focus();
+        // アニメーションを発生させる
+        if (to_element) {
+          const e = to_element.parentElement;
+          if (!e) return;
+          e.animate(
+            [{transform: `translateY(${(e.clientHeight + 20) * -direction}px)`}, {transform: 'translateY(0px)'}],
+            {duration: 200, easing: 'ease-out'},
+          );
+        }
+        if (from_element instanceof HTMLElement) {
+          const e = from_element.parentElement;
+          if (!e) return;
+          e.animate(
+            [{transform: `translateY(${(e.clientHeight + 20) * direction}px)`}, {transform: 'translateY(0px)'}],
+            {duration: 200, easing: 'ease-out'},
+          );
+        }
+      }
+    };
+
+    window.addEventListener('keydown', Shortcut);
+    return () => window.removeEventListener('keydown', Shortcut);
+  }, [state]);
+
+  // ページ移動時、フォーカスする
+  React.useEffect(() => {
+    switch (exam.type) {
+      case 'Text':
+        document.getElementById(TEXT_FIRST_FORM_ID)?.focus();
+        break;
+      case 'Select':
+      case 'MultiSelect':
+        document.getElementById(SELECT_FIRST_ITEM_ID)?.focus();
+        break;
+      case 'Sort':
+        document.getElementById(SORT_FIRST_ITEM_ID)?.focus();
+        break;
+    }
+  }, [exam]);
 
   function ResultIndicator(props: {index: number}): JSX.Element {
     let classname = css.result_indicator_before_check;
@@ -35,6 +121,7 @@ export function ExamAnswerArea(): JSX.Element {
               <div key={'text-' + i} className={css.item}>
                 <ResultIndicator index={i} />
                 <Form
+                  id={i === 0 ? TEXT_FIRST_FORM_ID : ''}
                   label={exam.answer.length === 1 ? '解答' : `解答 (${i + 1})`}
                   value={status.user_answer[i]}
                   disabled={status.checked}
@@ -56,6 +143,7 @@ export function ExamAnswerArea(): JSX.Element {
                   })()}
                 />
                 <SelectButton
+                  id={i === 0 ? SELECT_FIRST_ITEM_ID : ''}
                   type='radio'
                   desc={e}
                   status={status.user_answer[0] === String(i)}
@@ -68,6 +156,7 @@ export function ExamAnswerArea(): JSX.Element {
               <div key={'multiselect-' + i} className={css.item}>
                 <ResultIndicator index={i} />
                 <SelectButton
+                  id={i === 0 ? SELECT_FIRST_ITEM_ID : ''}
                   type='check'
                   desc={e}
                   status={status.user_answer.indexOf(String(i)) !== -1}
@@ -96,12 +185,12 @@ export function ExamAnswerArea(): JSX.Element {
                                 <ResultIndicator index={i} />
                                 <div
                                   className={css.sort_item}
-                                  id={i === 0 ? 'sort-first-draghandle' : ''}
+                                  id={i === 0 ? SORT_FIRST_ITEM_ID : ''}
                                   {...{sort_index: i}}
                                   {...provided.dragHandleProps}
                                 >
                                   <span>{e}</span>
-                                  <div className={css.drag_icon} {...provided.dragHandleProps}>
+                                  <div className={css.drag_icon}>
                                     <ListIcon />
                                   </div>
                                 </div>
