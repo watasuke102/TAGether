6b712d0cbfcfa5803c832f5b7fd3d33808581f95
watasuke102 <watasuke102@gmail.com>
2024-01-15 00:57:29 +0900

    update: profile>履歴をちゃんと表示できるようにした

--- src/components/features/ExamHistoryTable/ExamHistoryItem.tsx
+++ src/components/features/ExamHistoryTable/ExamHistoryItem.tsx
@@ -8,63 +8,70 @@ import css from './ExamHistoryItem.module.scss';
 import React from 'react';
 import Button from '@/common/Button/Button';
 import Modal from '@/common/Modal/Modal';
-import {AllCategoryDataType} from '@mytypes/Categoly';
-import ExamHistory from '@mytypes/ExamHistory';
-import Detail from '../CategolyCard/CategolyDetail';
+import {delete_history} from '@utils/api/history';
+import {AllHistory} from '@mytypes/ExamHistory';
 import DeleteIcon from '@assets/delete.svg';
 import CloseIcon from '@assets/close.svg';
+import ClockIcon from '@assets/clock.svg';
+import {useRouter} from 'next/navigation';
 
-interface Props {
-  categoly: AllCategoryDataType;
-  item: ExamHistory;
-  remove: () => void;
-}
-
-export default function ExamHistoryItem(props: Props): React.ReactElement {
+export function ExamHistoryItem(props: AllHistory): React.ReactElement {
   const [is_modal_open, SetIsModalOpen] = React.useState(false);
-  const [is_categoly_detail_open, SetIsCategolyDetailOpen] = React.useState(false);
-  const rate = Math.round((props.item.correct_count / props.item.total_question) * 10000) / 100;
+  const [correct_answers, total_questions] = props.exam_state.reduce(
+    (acc, cur) => [acc[0] + cur.correct_count, acc[1] + cur.total_question],
+    [0, 0],
+  );
+  const rate = Math.round((correct_answers / total_questions) * 10000) / 100;
+  const router = useRouter();
 
   return (
     <>
       <div className={css.container}>
-        <div className={css.delete_button_wrapper}>
+        <div className={css.delete_button}>
           <Button text='削除' icon={<DeleteIcon />} type='material' OnClick={() => SetIsModalOpen(true)} />
         </div>
 
-        <span className={css.categoly_link} onClick={() => SetIsCategolyDetailOpen(true)}>
-          {props.categoly.title}
+        <span className={css.title} onClick={() => router.push(`/examtable?history_id=${props.id}`)}>
+          {props.title}
         </span>
 
         <div className={css.status}>
           <span>
-            {props.item.total_question}問中{props.item.correct_count}問正解 ({isNaN(rate) ? 0 : rate}%)
+            {total_questions}問中{correct_answers}問正解 ({isNaN(rate) ? 0 : rate}%)
           </span>
-          <span className={css.date}>{props.item.categoly.updated_at ?? ''}</span>
+          <span className={css.date}>{props.created_at.slice(0, -5).replace('T', ' ')}</span>
+        </div>
+        <div className={css.redo_button}>
+          {total_questions === correct_answers ? (
+            <></>
+          ) : (
+            <Button
+              text='解き直し'
+              icon={<ClockIcon />}
+              type='material'
+              OnClick={() => router.push(`/exam?history_id=${props.id}`)}
+            />
+          )}
         </div>
       </div>
 
-      <Modal isOpen={is_categoly_detail_open} close={() => SetIsCategolyDetailOpen(false)}>
-        <Detail data={props.categoly} history={props.item} close={() => SetIsCategolyDetailOpen(false)} />
-      </Modal>
-
       <Modal isOpen={is_modal_open} close={() => SetIsModalOpen(false)}>
         <div className={css.modal}>
-          <span className={css.title}>{props.categoly.title}</span>
+          <span className={css.title}>{props.title}</span>
           <p>この解答履歴を削除しますか？</p>
           <div className={css.buttons}>
             <Button
               OnClick={() => {
                 SetIsModalOpen(false);
               }}
-              type='filled'
+              type='material'
               icon={<CloseIcon />}
               text='閉じる'
             />
             <Button
               OnClick={() => {
-                props.remove();
                 SetIsModalOpen(false);
+                delete_history(props.id);
               }}
               type='filled'
               icon={<DeleteIcon />}
