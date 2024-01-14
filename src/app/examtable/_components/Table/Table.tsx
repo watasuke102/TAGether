ca95c22437260c83bd2dc9525d6304b45f8dec53
watasuke102 <watasuke102@gmail.com>
2024-01-15 01:49:55 +0900

    update: examtable>履歴の内容をちゃんと表示できるようにした

--- src/app/examtable/_components/Table/Table.tsx
+++ src/app/examtable/_components/Table/Table.tsx
@@ -7,43 +7,41 @@
 'use client';
 import css from './Table.module.scss';
 import React from 'react';
-import Helmet from 'react-helmet';
 import {useRouter} from 'next/navigation';
 import Button from '@/common/Button/Button';
 import {SelectButton} from '@/common/SelectBox';
 import {FmtCorrectAnswer} from '@/features/Exam/FmtCorrectAnswer/FmtCorrectAnswer';
 import AnswerState from '@mytypes/AnswerState';
-import {CategoryDataType} from '@mytypes/Categoly';
 import Exam from '@mytypes/Exam';
-import ExamHistory from '@mytypes/ExamHistory';
+import {History} from '@mytypes/ExamHistory';
 import ArrowLeftIcon from '@assets/arrow-left.svg';
 import VisibleIcon from '@assets/visible.svg';
 import InvisibleIcon from '@assets/invisible.svg';
+import ArrowRightIcon from '@assets/arrow-right.svg';
 import {Shuffle} from '@utils/ArrayUtil';
+import {FmtUserAnswer} from '@/features/Exam/FmtUserAnswer/FmtUserAnswer';
 
-interface Props {
-  data: CategoryDataType;
-  history?: ExamHistory;
-}
+export type ExamTableProps = {
+  title: string;
+  exam: Exam[];
+  id?: string;
+  history?: History;
+};
 
-export function Table(props: Props): React.ReactElement {
+export function Table(props: ExamTableProps): React.ReactElement {
   const [show_correct_answer, SetShowCorrectAnswer] = React.useState(false);
+  const [show_user_answer, set_show_user_answer] = React.useState(false);
   const [filter, SetFilter] = React.useState(0x07);
   const router = useRouter();
 
-  const UpdateFilter = (type: AnswerState, state: boolean) => {
-    SetFilter(filter => {
-      if (state) {
-        return filter | type;
-      } else {
-        return filter & (~type & 0x07);
-      }
-    });
-  };
+  const [correct_answers, total_questions] = props.history?.exam_state.reduce(
+    (acc, cur) => [acc[0] + cur.correct_count, acc[1] + cur.total_question],
+    [0, 0],
+  ) ?? [undefined, undefined];
 
   const choices: string[] = React.useMemo(
     () =>
-      (JSON.parse(props.data.list) as Exam[]).map(exam => {
+      props.exam.map(exam => {
         let array: string[];
         switch (exam.type) {
           case 'Select':
@@ -62,13 +60,23 @@ export function Table(props: Props): React.ReactElement {
       }),
     [],
   );
+  const answer_state = React.useMemo(
+    () =>
+      props.history?.exam_state.map(e => {
+        if (e.correct_count === e.total_question) {
+          return AnswerState.AllCorrect;
+        } else if (e.correct_count === 0) {
+          return AnswerState.AllWrong;
+        }
+        return AnswerState.PartialCorrect;
+      }),
+    [],
+  );
 
   return (
     <>
-      <Helmet title={`問題一覧 : ${props.data.title} - TAGether`} />
-
       <div className={css.examdata_container}>
-        <span className={css.title}>{props.data.title}</span>
+        <span className={css.title}>{props.title}</span>
         {props.history && (
           <>
             <div className={css.filter_selector}>
@@ -76,25 +84,27 @@ export function Table(props: Props): React.ReactElement {
                 type='check'
                 desc='全問正解'
                 status={(filter & AnswerState.AllCorrect) !== 0}
-                onChange={f => UpdateFilter(AnswerState.AllCorrect, f)}
+                onChange={() => SetFilter(f => f ^ AnswerState.AllCorrect)}
               />
               <SelectButton
                 type='check'
                 desc='部分正解'
                 status={(filter & AnswerState.PartialCorrect) !== 0}
-                onChange={f => UpdateFilter(AnswerState.PartialCorrect, f)}
+                onChange={() => SetFilter(f => f ^ AnswerState.PartialCorrect)}
               />
               <SelectButton
                 type='check'
                 desc='不正解'
                 status={(filter & AnswerState.AllWrong) !== 0}
-                onChange={f => UpdateFilter(AnswerState.AllWrong, f)}
+                onChange={() => SetFilter(f => f ^ AnswerState.AllWrong)}
               />
             </div>
-            <span>
-              {props.history.total_question}問中{props.history.correct_count}問正解、 正答率
-              {props.history && Math.round((props.history.correct_count / props.history.total_question) * 10000) / 100}%
-            </span>
+            {total_questions !== undefined && correct_answers !== undefined && (
+              <span>
+                {total_questions}問中{correct_answers}問正解、 正答率
+                {props.history && Math.round((correct_answers / total_questions) * 10000) / 100}%
+              </span>
+            )}
           </>
         )}
       </div>
@@ -104,15 +114,15 @@ export function Table(props: Props): React.ReactElement {
           <tr>
             <th>問題</th>
             <th>正解</th>
-            <th>コメント</th>
             {props.history && (
               <>
                 <th>自分の解答</th>
                 <th className={css.result}>結果</th>
               </>
             )}
+            <th>コメント</th>
           </tr>
-          {(JSON.parse(props.data.list) as Exam[])
+          {props.exam
             .map((exam, i) => (
               <tr key={`tr-${i}`}>
                 <td>
@@ -129,11 +139,14 @@ export function Table(props: Props): React.ReactElement {
                 <td className={show_correct_answer ? '' : css.hide}>
                   <FmtCorrectAnswer exam={exam} />
                 </td>
-                <td className={show_correct_answer ? '' : css.hide}>{exam.comment ?? ''}</td>
                 {props.history && (
                   <>
-                    <td>
-                      <FmtCorrectAnswer exam={exam} result={props.history.exam_state[i].result} />
+                    <td className={show_user_answer ? '' : css.hide}>
+                      <FmtUserAnswer
+                        exam={exam}
+                        user_answer={props.history.exam_state[i].user_answer}
+                        result={props.history.exam_state[i].result}
+                      />
                     </td>
                     <td>
                       {(() => {
@@ -149,21 +162,35 @@ export function Table(props: Props): React.ReactElement {
                     </td>
                   </>
                 )}
+                <td className={show_correct_answer ? '' : css.hide}>{exam.comment ?? ''}</td>
               </tr>
             ))
-            .filter((_, i) => !props.history || (filter & props.history?.exam_state[i].order) !== 0)}
+            .filter((_, i) => !props.history || (filter & (answer_state?.at(i) ?? 0xff)) !== 0)}
         </tbody>
       </table>
 
       <div className={css.button_container}>
         <div className={css.buttons}>
           <Button text='戻る' icon={<ArrowLeftIcon />} OnClick={router.back} type='material' />
-          {/* 正しい答えの表示/非表示切り替え */}
+          <Button
+            OnClick={() => set_show_user_answer(e => !e)}
+            type='material'
+            text={show_user_answer ? '自分の解答を非表示' : '自分の解答を表示'}
+            icon={show_user_answer ? <InvisibleIcon /> : <VisibleIcon />}
+          />
           <Button
             OnClick={() => SetShowCorrectAnswer(!show_correct_answer)}
             type='material'
             text={show_correct_answer ? '正解を非表示' : '正解を表示'}
             icon={show_correct_answer ? <InvisibleIcon /> : <VisibleIcon />}
+          />
+          <Button
+            OnClick={() =>
+              router.push(props.history ? `/exam?history_id=${props.history.id}` : `/exam?id=${props.id ?? ''}`)
+            }
+            type='filled'
+            text={'この問題を解く'}
+            icon={<ArrowRightIcon />}
           />
         </div>
       </div>
---
+++ src/components/features/Exam/FmtUserAnswer/FmtUserAnswer.tsx
@@ -0,0 +1,80 @@
+// TAGether - Share self-made exam for classmates
+// CopyRight (c) 2020-2024 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT or MIT SUSHI-WARE License.
+import css from './FmtUserAnswer.module.scss';
+import React from 'react';
+import Exam from '@mytypes/Exam';
+
+type Props = {
+  exam: Exam;
+  user_answer: string[];
+  result: boolean[];
+};
+
+export function FmtUserAnswer(props: Props): JSX.Element {
+  return (
+    <>
+      {(() => {
+        switch (props.exam.type ?? 'Text') {
+          case 'Text':
+            if (props.exam.answer.length === 1) {
+              return <span className={props.result?.at(0) ?? true ? '' : css.wrong}>{props.user_answer[0]}</span>;
+            }
+            return (
+              <ol>
+                {props.user_answer.map((e, i) => (
+                  <li key={'result_select_' + i} className={props.result?.at(i) ?? true ? '' : css.wrong}>
+                    {e}
+                  </li>
+                ))}
+              </ol>
+            );
+          case 'Select':
+            return (
+              <span className={props.result?.at(0) ?? true ? '' : css.wrong}>
+                {props.exam.question_choices?.at(Number(props.exam.answer[0]))}
+              </span>
+            );
+          case 'MultiSelect':
+            return (
+              <ul>
+                {props.exam.answer
+                  .filter((_m, i) => props.user_answer[i] === String(i))
+                  .map((e, i) => (
+                    <li key={'result_multiselect_' + i} className={props.result?.at(i) ?? true ? '' : css.wrong}>
+                      {e}
+                    </li>
+                  ))}
+              </ul>
+            );
+          case 'Sort':
+            return (
+              <ol>
+                {props.user_answer.map((e, i) => (
+                  <li key={'result_sort_' + i} className={props.result?.at(i) ?? true ? '' : css.wrong}>
+                    {e}
+                  </li>
+                ))}
+              </ol>
+            );
+          case 'ListSelect':
+            return (
+              <ol>
+                {props.user_answer.map((e, i) => (
+                  <li key={'result_listselect_' + i} className={props.result?.at(i) ?? true ? '' : css.wrong}>
+                    {e}
+                  </li>
+                ))}
+              </ol>
+            );
+
+          default:
+            throw Error('invalid Exam type');
+        }
+      })()}
+    </>
+  );
+}
