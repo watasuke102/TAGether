516a2044bff1153e165b8753cf5270a48b19f0c4
watasuke102 <watasuke102@gmail.com>
2024-01-15 00:24:01 +0900

    update: お気に入りの編集を出来るようにした

---
+++ src/app/api/user/favorite/route.ts
@@ -0,0 +1,24 @@
+// TAGether - Share self-made exam for classmates
+// CopyRight (c) 2020-2024 watasuke
+//
+// Email  : <watasuke102@gmail.com>
+// Twitter: @Watasuke102
+// This software is released under the MIT or MIT SUSHI-WARE License.
+import {users} from 'src/db/schema';
+import {connect_drizzle} from 'src/db/drizzle';
+import {eq} from 'drizzle-orm';
+import {getIronSession} from 'iron-session';
+import {Session} from '@mytypes/Session';
+import {cookies} from 'next/headers';
+import {env} from 'env';
+
+export async function PUT(req: Request): Promise<Response> {
+  const session = await getIronSession<Session>(cookies(), env.SESSION_OPTION);
+  if (session.is_logged_in !== true) {
+    return Response.json({}, {status: 401});
+  }
+  const favorite_list: string = await req.text();
+  const db = await connect_drizzle();
+  await db.update(users).set({favorite_list}).where(eq(users.uid, session.uid));
+  return Response.json({});
+}
--- src/utils/api/user.ts
+++ src/utils/api/user.ts
@@ -4,9 +4,19 @@
 // Email  : <watasuke102@gmail.com>
 // Twitter: @Watasuke102
 // This software is released under the MIT or MIT SUSHI-WARE License.
+import {mutate} from 'swr';
 import {User} from '@mytypes/User';
 import {useApiData} from './common';
+import axios from 'axios';
+import {ToggleElement} from '@utils/ArrayUtil';
 
 const user_key = '/api/user';
 
+export const mutate_user = (): Promise<void> => mutate(user_key);
 export const useUser = useApiData<User>(user_key);
+
+export async function toggle_favorite(current: User, category_id: number): Promise<void> {
+  const new_favorite_list = ToggleElement(current.favorite_list, category_id);
+  await axios.put(`${user_key}/favorite`, JSON.stringify(new_favorite_list));
+  mutate_user();
+}
