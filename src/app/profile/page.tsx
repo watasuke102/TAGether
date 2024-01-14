d0f294392fd62581ca1b4c2c4c0253b354012c78
watasuke102 <watasuke102@gmail.com>
2024-01-15 00:57:29 +0900

    update: profile>履歴をちゃんと表示できるようにした

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -13,19 +13,16 @@ import {IndexedContainer} from '@/common/IndexedContainer';
 import Loading from '@/common/Loading/Loading';
 import Modal from '@/common/Modal/Modal';
 import CategolyCard from '@/features/CategolyCard/CategolyCard';
-import HistoryTable from '@/features/ExamHistoryTable/ExamHistoryItem';
+import {ExamHistoryItem} from '@/features/ExamHistoryTable/ExamHistoryItem';
 import {logout, useSession} from '@utils/api/session';
 import {useUser} from '@utils/api/user';
 import {useAllCategoryData} from '@utils/api/category';
-import DeleteIcon from '@assets/delete.svg';
-import CloseIcon from '@assets/close.svg';
 import ArrowLeftIcon from '@assets/arrow-left.svg';
 import {useAllHistory} from '@utils/api/history';
 
 export default function profile(): React.ReactElement {
   useSession(true);
   const [user, is_user_loading] = useUser();
-  const [is_modal_open, SetIsModalOpen] = React.useState(false);
   const [histories, is_history_loading] = useAllHistory();
   const [data, isLoading] = useAllCategoryData();
 
@@ -62,72 +59,16 @@ export default function profile(): React.ReactElement {
         <hr />
 
         <h2>解答履歴</h2>
-        <div className={css.buttons}>
-          <div className={css.allclear_button}>
-            <Button
-              {...{
-                text: '履歴を全消去',
-                icon: <DeleteIcon />,
-                OnClick: () => SetIsModalOpen(true),
-                type: 'filled',
-              }}
-            />
-          </div>
-        </div>
-
         {is_history_loading ? (
           <Loading />
         ) : (
           <IndexedContainer len={histories.length} per={8}>
             {histories.map(item => {
-              return (
-                <div key={item.id} style={{whiteSpace: 'pre-wrap'}}>
-                  {JSON.stringify(item, undefined, '  ')}
-                </div>
-                // <HistoryTable
-                //   key={`history_${item.history_key}`}
-                //   item={item}
-                //   categoly={item.categoly}
-                //   remove={() => {
-                //     RemoveExamHistory(item.history_key ?? '');
-                //     const tmp = history_list.concat();
-                //     // history_keyが存在しない場合は何も削除しない
-                //     tmp.splice(Number(item.history_key ?? tmp.length), 1);
-                //     SetHistoryList(tmp);
-                //   }}
-                // />
-              );
+              return <ExamHistoryItem key={`history_${item.id}`} {...item} />;
             })}
           </IndexedContainer>
         )}
       </div>
-
-      <Modal isOpen={is_modal_open} close={() => SetIsModalOpen(false)}>
-        <div className={css.modal}>
-          <p>解答履歴をすべて削除しますか？</p>
-          <div className={css.window_buttons}>
-            <Button
-              {...{
-                type: 'material',
-                icon: <CloseIcon />,
-                text: '閉じる',
-                OnClick: () => SetIsModalOpen(false),
-              }}
-            />
-            <Button
-              {...{
-                OnClick: () => {
-                  ClearExamHistory().then(InitExamHistory);
-                  SetIsModalOpen(false);
-                },
-                type: 'filled',
-                icon: <DeleteIcon />,
-                text: '削除する',
-              }}
-            />
-          </div>
-        </div>
-      </Modal>
     </>
   );
 }
