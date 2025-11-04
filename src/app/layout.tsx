4b4c843596a8e4b00680cd2b6a59ad8c3e7c1612
watasuke102 <watasuke102@gmail.com>
2025-11-04 16:27:17 +0900

    update: フォントとヘッダ周辺の見た目を修正
    Web Fontを使わないようにした

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,22 +6,19 @@
 // This software is released under the MIT or MIT SUSHI-WARE License.
 import React from 'react';
 import '@/common/main.scss';
-import {Toast, ToastProvider} from '@/common/Toast/Toast';
+import { Toast, ToastProvider } from '@/common/Toast/Toast';
 
 export const metadata = {
   title: 'TAGether',
 };
 
-export default function RootLayout({children}: {children: React.ReactNode}): JSX.Element {
+export default function RootLayout({ children }: { children: React.ReactNode }): JSX.Element {
   return (
     <html lang='ja'>
       <head>
         <link rel='icon' href='/static/icon.ico' />
         <link rel='apple-touch-icon' href='/static/icon-maskable.png' sizes='480x480' />
         <link rel='manifest' href='/manifest.json' />
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' crossOrigin='anonymous' />
-        <link href='https://fonts.googleapis.com/css2?family=M+PLUS+1p&display=swap' rel='stylesheet' />
       </head>
       <body>
         <ToastProvider>
